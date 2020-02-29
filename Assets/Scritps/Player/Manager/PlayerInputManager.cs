0bbe25ad6bfab4c9195e15022944aaec247ed1e4
Temus <temus@wir.pl>
2020-02-29 10:04:15 +0100

    Add ship rotating

---
+++ Assets/Scritps/Player/Manager/PlayerInputManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    [SerializeField] private Transform mapCenterPoint;
+    [SerializeField] private GameObject playerShip;
+
+    private float speed = 100f;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            RotateShip(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            RotateShip(Vector3.back);
+        }
+    }
+
+    private void RotateShip(Vector3 rotateAxis)
+    {
+        playerShip.transform.RotateAround(Vector3.zero,rotateAxis, Time.deltaTime * speed);
+    }
+    
+    
+    
+}
