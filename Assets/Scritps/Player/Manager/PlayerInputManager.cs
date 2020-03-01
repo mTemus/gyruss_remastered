98aee1d07a4bba7a085b4fd78ee765fcf0abd4b9
Temus <temus@wir.pl>
2020-03-01 17:07:39 +0100

    Add automatic spawn player in starting position

--- Assets/Scritps/Player/Manager/PlayerInputManager.cs
+++ Assets/Scritps/Player/Manager/PlayerInputManager.cs
@@ -1,15 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerInputManager : MonoBehaviour
 {
     [SerializeField] private GameObject playerShip;
+    [SerializeField] private SpriteRenderer background;
 
-    private float speed = 100f;
+    private float speed = 150f;
     
     void Start()
     {
+        Camera cam = Camera.main;
+        Vector3 startingPosition = Vector3.zero;
+        
+        // if (cam.pixelWidth < cam.pixelHeight) {
+            // Vector3 p1 = cam.WorldToScreenPoint(Vector3.zero);
+            // Vector3 p2 = cam.WorldToScreenPoint(Vector3.right);
+            // float dist = Vector3.Distance(p1, p2);
+            // float halfOfWidth = (cam.pixelWidth * (1 / dist)) - 0.5f;
+            
+            // startingPosition = new Vector3(0, -(halfOfWidth / 2), 0);
+        // }
+        // else if (cam.pixelWidth > cam.pixelHeight) {
+        //     Vector3 p1 = cam.WorldToScreenPoint(Vector3.zero);
+        //     Vector3 p2 = cam.WorldToScreenPoint(Vector3.up);
+        //     float dist = Vector3.Distance(p1, p2);
+        //     float halfOfHeight = (cam.pixelHeight * (1 / dist)) - 0.5f;
+        //     
+        //     startingPosition = new Vector3(0, -(halfOfHeight / 2), 0);
+        // }
+
+        Bounds backgroundBounds = background.bounds;
+        startingPosition = new Vector3(0, -((backgroundBounds.size.y / 4) - 0.2f), 0);
+        playerShip.transform.position = startingPosition;
+
 
     }
 
