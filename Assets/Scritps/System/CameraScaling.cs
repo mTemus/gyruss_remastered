1fac9a1f2ab3738cabe003c165fbef82d3092351
Temus <temus@wir.pl>
2020-03-01 17:06:56 +0100

    Add scaling camera after changing screen ratio

---
+++ Assets/Scritps/System/CameraScaling.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraScaling : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bounds backgroundBounds = background.bounds;
+        float screenRatio = (float) Screen.width / (float) Screen.height;
+        float targetRatio = backgroundBounds.size.x / backgroundBounds.size.y;
+
+        if (screenRatio >= targetRatio) { Camera.main.orthographicSize = background.bounds.size.y / 2; }
+        else {
+            float differenceInSize = targetRatio / screenRatio;
+            Camera.main.orthographicSize = background.bounds.size.y / 2 * differenceInSize;
+        }
+    }
+}
