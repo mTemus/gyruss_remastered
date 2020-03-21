591dce6fa7ff8d5f2267093f14e45a2aa331cb98
Temus <temus@wir.pl>
2020-03-21 15:22:15 +0100

    Fix double shooting scaling

--- Assets/Scritps/Player/Controller/BulletController.cs
+++ Assets/Scritps/Player/Controller/BulletController.cs
@@ -3,35 +3,22 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    [SerializeField] private float speed = 1;
-    [SerializeField] private Vector3 bulletScale = new Vector3(0.0001f, 0.0001f, 0);
+    [SerializeField] private float speed = 5f;
 
-    private Transform bulletTransform;
+    private readonly Vector3 scaling = new Vector3(0.01f,0.01f,0);
     
-    
-    private void Start()
-    {
-        bulletTransform = transform;
-    }
-
     void Update()
     {
         if (transform.gameObject.activeSelf)
         {
             transform.position = Vector3.MoveTowards(transform.position, Vector3.zero, Time.deltaTime * speed);
 
-            // if (transform.localScale.x > 0)
-            //     transform.localScale -= bulletScale;
-            //
+            if (transform.localScale.x > 0)
+                transform.localScale -= scaling;
+            
             if (transform.position == Vector3.zero)
                 Destroy(transform.gameObject);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        // if (other.CompareTag("MapCenter")) {
-        //     Destroy(transform.gameObject);
-        // }
-    }
 }
