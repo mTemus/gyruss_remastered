036abb2f7a9fad2938fed837f7fffc856aa78f0d
Temus <temus@wir.pl>
2020-03-22 17:34:14 +0100

    Add enemy spots mechanism

--- Assets/Scritps/Game/Manager/StageManager.cs
+++ Assets/Scritps/Game/Manager/StageManager.cs
@@ -1,18 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
+    [SerializeField] private Transform[] enemySpots;
+
+    private Dictionary<Transform, bool> enemySpotsMap;
+
+    private int currentStage = 1;
+    private int enemiesAlive;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        enemySpotsMap = new Dictionary<Transform, bool>();
         
+        InitiateSpotsMap();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
+    
+    private void InitiateSpotsMap()
+    {
+        foreach (Transform enemySpot in enemySpots) { enemySpotsMap[enemySpot] = false; }
+    }
+
+    private void SetEnemySpotOccupied(int index)
+    {
+        Transform t = enemySpots[index];
+        enemySpotsMap[t] = true;
+    }
+
+    private void SetEnemySpotFree(int index)
+    {
+        Transform t = enemySpots[index];
+        enemySpotsMap[t] = false;
+    }
+
+    private Vector3 GetEnemySpotPosition(int index)
+    {
+        Transform t = enemySpots[index];
+
+        if (enemySpotsMap[t]) return Vector3.negativeInfinity;
+        
+        SetEnemySpotOccupied(index);
+        return t.position;
+    }
+
+    private void AddEnemiesAlive(int enemiesAmount)
+    {
+        enemiesAlive += enemiesAmount;
+    }
+
+
+    public int CurrentStage => currentStage;
+
+    public int EnemiesAlive => enemiesAlive;
 }
