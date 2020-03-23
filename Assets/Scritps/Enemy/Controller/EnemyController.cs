7812d31305cd862d9e92cc9123886008e5f80d64
matmaj81 <matmaj81@wp.pl>
2020-03-23 20:14:18 +0100

    enemy state machine controller initialization

--- Assets/Scritps/Enemy/Controller/EnemyController.cs
+++ Assets/Scritps/Enemy/Controller/EnemyController.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    
+    public EnemyStates currentEnemyState = EnemyStates.entering;
+
+    private void Update() {
+        switch(currentEnemyState){
+            case EnemyStates.entering:
+                enterScreen();
+            break;
+            case EnemyStates.fly_to_spot:
+                flyToTheCenter();
+            break;
+            case EnemyStates.wait:
+                waitInTheMiddle();
+            break;
+            case EnemyStates.attack:
+                attackPlayer();
+            break;
+            case EnemyStates.fly_away:
+                flyAway();
+            break;
+        }   
+    }
+
+    private void enterScreen(){
+
+    }
+
+    private void flyToTheCenter(){
+
+    }
+
+    private void waitInTheMiddle(){
+
+    }
+
+    private void attackPlayer(){
+
+    }
+
+    private void flyAway(){
+
+    }
 }
