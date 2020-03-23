2364bd1a6ebbfe1c331957ca63e1104ea061bcb6
Temus <temus@wir.pl>
2020-03-23 08:26:09 +0100

    Update enemy AI state machine

--- Assets/Scritps/Enemy/Model/EnemyStates.cs
+++ Assets/Scritps/Enemy/Model/EnemyStates.cs
@@ -7,4 +7,5 @@
     fly_away, // (chance stage) when enters, fly some distance on a path and fly away
     fly_to_mini_boss, // (mini_boss stage) fly to mini_boss to be eaten
     fly_from_mini_boss, // fly from mini_boss and attack
+    die // opening explosion animation
 }
