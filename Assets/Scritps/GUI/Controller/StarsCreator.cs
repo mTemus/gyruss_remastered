4a57b4aef499e49eb23eb3858f6647e903df16c3
Temus <temus@wir.pl>
2020-04-03 16:36:32 +0200

    Add map of levels in the game

---
+++ Assets/Scritps/GUI/Controller/StarsCreator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class StarsCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject starPrefab;
+    [SerializeField] private GameObject starsBackground;
+    [SerializeField] private int starsAmount = 100;
+    
+    private float sizeX;
+    private float sizeY;
+    
+    
+    
+    void Start()
+    {
+        Vector3 backgroundScale = starsBackground.transform.localScale;
+        sizeX = backgroundScale.x;
+        sizeY = backgroundScale.y;
+
+        for (int i = 0; i < starsAmount; i++) {
+            float posX = Random.Range(-(sizeX / 2), sizeX / 2);
+            float posY = Random.Range(-(sizeY / 2), sizeY / 2);
+
+            GameObject star = Instantiate(starPrefab, starsBackground.transform, true);
+            star.transform.position = new Vector3(posX, posY, 2);
+        }
+    }
+
+    
+}
