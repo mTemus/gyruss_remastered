5babab3285b0ea769ef9d86d782036035a9a9c70
Temus <temus@wir.pl>
2020-04-11 11:57:39 +0200

    Add shaking effect to center points

---
+++ Assets/Scritps/Map/Controller/PointShaker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PointShaker : MonoBehaviour
+{
+    [SerializeField] private float shakeArea = 0.3f;
+    [SerializeField] private float shakeSpeed = 0.01f;
+
+    private int index;
+    private float currentShake;
+    
+    void Start()
+    {
+        string pointName = transform.name;
+        index = int.Parse(pointName[pointName.Length - 1].ToString());
+    }
+
+    void Update()
+    {
+        var myTransform = transform;
+        Vector3 currPos = myTransform.position;
+
+        myTransform.position = index % 2 == 0 ? 
+            new Vector3(currPos.x, currPos.y + shakeSpeed, currPos.z) :
+            new Vector3(currPos.x + shakeSpeed, currPos.y, currPos.z);
+        
+        currentShake += shakeSpeed;
+
+        if (!(Mathf.Abs(currentShake) >= shakeArea)) return;
+        currentShake = 0;
+        shakeSpeed = -shakeSpeed;
+    }
+}
