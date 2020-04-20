6688858f5404b2dd2904ac886063aea6c8e9e1aa
Temus <temus@wir.pl>
2020-04-20 12:53:38 +0200

    Add sound files

---
+++ Assets/Scritps/Sound/Manager/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private SingleSound[] soundEffects;
+    [SerializeField] private SingleSound[] soundMusic;
+
+    private void Awake()
+    {
+        foreach (SingleSound sound in soundEffects) {
+            sound.Source = gameObject.AddComponent<AudioSource>();
+
+            sound.Source.clip = sound.Clip;
+            sound.Source.volume = sound.Volume;
+            sound.Source.pitch = sound.Pitch;
+            sound.Source.loop = sound.Loop;
+        }
+        
+        foreach (SingleSound sound in soundMusic) {
+            sound.Source = gameObject.AddComponent<AudioSource>();
+
+            sound.Source.clip = sound.Clip;
+            sound.Source.volume = sound.Volume;
+            sound.Source.pitch = sound.Pitch;
+            sound.Source.loop = sound.Loop;
+        }
+    }
+}
---
+++ Assets/Scritps/Sound/Model/SingleSound.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[Serializable]
+public class SingleSound
+{
+    [Header("Sound data")] 
+    [SerializeField] private string name;
+    [SerializeField] private AudioClip clip;
+
+    [Header("Sound properties")]
+    [SerializeField] [Range(0f, 1f)] private float volume = 1;
+    [SerializeField] [Range(0.1f, 3f)]private float pitch = 0.1f;
+    [SerializeField] private bool loop;
+    
+    
+    private AudioSource source;
+
+    public string Name => name;
+
+    public AudioClip Clip => clip;
+
+    public float Pitch => pitch;
+
+    public float Volume => volume;
+
+    public bool Loop => loop;
+
+    public AudioSource Source
+    {
+        get => source;
+        set => source = value;
+    }
+}
